26d7da68d9716eeea41332069643283678e046fc
DanielR2132 <154104609+DanielR2132@users.noreply.github.com>
2024-02-07 18:02:57 -0600

    finished

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -23,36 +23,125 @@ function oldScrabbleScorer(word) {
 		 if (oldPointStructure[pointValue].includes(word[i])) {
 			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
 		 }
- 
 	  }
 	}
 	return letterPoints;
  }
 
+ function transform(oldPointStructure) {
+   let newPointStruct={};
+   let newKey = "";
+   
+   for (const key in oldPointStructure) {     
+       
+       for(let i=0;i< oldPointStructure[key].length;i++){   
+         newKey = oldPointStructure[key][i];          
+         
+         newPointStruct[newKey.toLowerCase()] =  Number(key);         
+       }     
+   }  
+   return newPointStruct;
+};
+
+let newPointStructure = transform(oldPointStructure);
+newPointStructure['']=0;
+
+function containsSpecialChars(string){
+   const specialChars = "~`#%^&*+=-[]\';/{}|\":<?";
+   for(i = 0; i < specialChars.length;i++){
+     if(string.indexOf(specialChars[i]) > -1){
+         return true;
+      }
+   }
+   return false;
+  }
+
+function containsNumber(str){   
+   return /\d/.test(str);
+}
+
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
+
+let word= '';
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   word = input.question("Let's play some scrabble!\nEnter a word: ");
+   while(containsSpecialChars(word) || containsNumber(word)){
+    word = input.question("Enter valid word with only letters: ");
+   }
+  return word;
 };
 
-let simpleScorer;
 
-let vowelBonusScorer;
+let simpleScorer = function(word){   
+  	return word.length;
+}
 
-let scrabbleScorer;
+let vowelBonusScorer= function(word){
+   let vowels = ['A','E','I','O','U'];
+   word = word.toUpperCase();
+   let letterPoints=0;
 
-const scoringAlgorithms = [];
+   for (let i = 0; i < word.length; i++) {
+      if(vowels.includes(word[i])){
+         letterPoints += 3;
+      }else{
+         letterPoints += 1;
+      }        
+    }  
+    return letterPoints;
+   }
 
-function scorerPrompt() {}
+let scrabbleScorer = function(word){
+   word = word.toLowerCase();
+	let letterPoints = 0;
+   for (let i = 0; i < word.length; i++) {  
+       if (word[i] in newPointStructure) {
+			letterPoints += newPointStructure[word[i]];
+		 }  
+	}  
+   
+	return letterPoints;
+}
 
-function transform() {};
+const scoringAlgorithms = [
+   {
+      name:"Simple Score",
+      description: "Each letter is worth 1 point!",
+      scorerFunction: simpleScorer
+   },
+   {
+      name:"Bonus Vowels",
+      description: "Vowels are 3 pts, consonants are 1 point!",
+      scorerFunction: vowelBonusScorer
+   },
+   {
+      name:"Scrabble",
+      description: "The traditional scoring algorithm!",
+      scorerFunction: scrabbleScorer
+   }];
 
-let newPointStructure;
+
+
+function scorerPrompt() {
+  
+   console.log(`\nWhich scoring algorithm would you like to use?\n`);
+
+   for(let i=0;i<scoringAlgorithms.length;i++){
+      console.log(`${i} - ${scoringAlgorithms[i].name}: ${scoringAlgorithms[i].description}`);
+   }
+   let scoringOption = input.question(`Enter 0, 1, or 2: `);
+   while(scoringOption.match(/[A-Za-z]/) || scoringOption < 0 || scoringOption > 2 || containsSpecialChars(scoringOption)){
+      scoringOption = input.question(`Enter valid scoring alogorithm option:`);
+   }
+   console.log(`Score for '${word}': ${scoringAlgorithms[scoringOption].scorerFunction(word)}`)  
+}
+
 
 function runProgram() {
    initialPrompt();
-   
+   scorerPrompt();
 }
 
 // Don't write any code below this line //
